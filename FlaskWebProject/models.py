d42dae86e680a66ac0d5a8929d06a86f4b3e040f
Kiruthika3478 <143984636+Kiruthika3478@users.noreply.github.com>
2026-04-10 12:07:02 +0530

    Update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -2,25 +2,32 @@ from datetime import datetime
 from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from azure.storage.blob import BlockBlobService
+from azure.storage.blob import BlobServiceClient
 import string, random
 from werkzeug.utils import secure_filename
 from flask import flash
 
+# Azure Blob configuration
 blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
+blob_service_client = BlobServiceClient.from_connection_string(
+    app.config['BLOB_CONNECTION_STRING']
+)
+container_client = blob_service_client.get_container_client(blob_container)
+
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f'<User {self.username}>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -28,12 +35,15 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+
 class Post(db.Model):
     __tablename__ = 'posts'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
     author = db.Column(db.String(75))
@@ -43,7 +53,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return f'<Post {self.id}>'
 
     def save_changes(self, form, file, userId, new=False):
         self.title = form.title.data
@@ -52,17 +62,24 @@ class Post(db.Model):
         self.user_id = userId
 
         if file:
-            filename = secure_filename(file.filename);
-            fileextension = filename.rsplit('.',1)[1];
-            Randomfilename = id_generator();
-            filename = Randomfilename + '.' + fileextension;
+            filename = secure_filename(file.filename)
+            extension = filename.rsplit('.', 1)[1]
+            filename = f"{id_generator()}.{extension}"
+
             try:
-                blob_service.create_blob_from_stream(blob_container, filename, file)
-                if(self.image_path):
-                    blob_service.delete_blob(blob_container, self.image_path)
-            except Exception:
-                flash(Exception)
-            self.image_path =  filename
+                blob_client = container_client.get_blob_client(filename)
+                blob_client.upload_blob(file, overwrite=True)
+
+                if self.image_path:
+                    old_blob_client = container_client.get_blob_client(self.image_path)
+                    old_blob_client.delete_blob()
+
+                self.image_path = filename
+
+            except Exception as e:
+                flash(str(e))
+
         if new:
             db.session.add(self)
+
         db.session.commit()
